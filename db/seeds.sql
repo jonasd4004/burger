9693359804d9d14562f89bb75f142cd4d278f0cf
Jonas Davidow <jonas.davidow@gmail.com>
2018-12-13 22:53:43 -0600

    recent updates

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name) VALUES("Turkey Burger");
+INSERT INTO burgers(burger_name) VALUES("Veggie Black Bean Burger");
+INSERT INTO burgers(burger_name) VALUES("Double Bacon Wrapped Cheeseburger");
